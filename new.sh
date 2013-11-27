52a96991a0ff3d0f03cc9822934a88c29087d16f
jackhanford <jackhanford@gmail.com>
2013-11-26 18:36:03 -0800

    First commit

---
+++ new.sh
@@ -0,0 +1,27 @@
+su
+ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"
+brew install automake
+brew install wget
+brew install git
+echo "Jack's Download for new machines"
+echo "Today is $(date)"
+mkdir Downloads
+cd Downloads
+  wget http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.dmg
+  wget https://download-chromium.appspot.com/dl/mac
+  wget http://iterm2.com/downloads/stable/iTerm2_v1_0_0.zip
+  wget http://81.169.182.62/smcfancontrol_2_1.zip
+  wget https://s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.dmg
+  wget http://cachefly.alfredapp.com/Alfred_2.1.1_227.zip
+  echo "Downlaods are complete!!!!!!!!"
+  sleep 3
+  echo "unzipping"
+  unzip mac
+  unzip iTerm2_v1_0_0.zip
+  unzip smcfancontrol_2_1.zip
+  unzip Alfred_2.1.1_227.zip
+  "unzipping complete, moving to downloads"
+  sleep 3
+    cd ../
+    mv Downloads ../Downloads
+echo "Completed!"
