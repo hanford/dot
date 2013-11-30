39bc5c9e87f8aa0758fe5ad7c78fa94c1fec76b7
jackhanford <jackhanford@gmail.com>
2013-11-30 11:31:22 -0800

    Should be all done?

--- new.sh
+++ new.sh
@@ -1,13 +1,20 @@
+function pause(){
+   read -p "$*"
+}
+
+pause 'Press [enter] to begin the set up script'
+echo "Making Projects folder.."
 mkdir Projects
 cd Projects
 echo "Attempting to SU"
 su
-#ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"
-#brew install automake
-#brew install wget
-#brew install git
-echo "Jack's Download for new machines"
-echo "Today is $(date)"
+ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"
+brew install automake
+brew install wget
+brew install git
+echo "Homebrew install should be done.. opening git repos to clone"
+open http://pastie.org/8519392
+sleep 5
 mkdir Downloads
 cd Downloads
   wget http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.dmg
@@ -17,8 +24,8 @@ cd Downloads
   wget https://s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.dmg
   wget http://cachefly.alfredapp.com/Alfred_2.1.1_227.zip
   open http://www.introversion.co.uk/prisonarchitect/builds/prisonarchitect-alpha15-macosx.zip
-  echo "Downlaods are complete!!!!!!!!"
-  sleep 3
+  echo "Downlaods are complete!!"
+  sleep 5
   echo "unzipping"
   unzip mac
   unzip iTerm2_v1_0_0.zip
